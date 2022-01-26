9f84dfb88113e296a66f07d67b5acc05542f7401
rhernandez <rhernandez@softura.com.mx>
2022-01-26 14:19:48 -0600

    First commit

---
+++ src/main/java/mx/com/softura/cursoSpringBot/CursoSpringBotApplication.java
@@ -0,0 +1,13 @@
+package mx.com.softura.cursoSpringBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/softura/cursoSpringBot/CursoSpringBotApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.softura.cursoSpringBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
